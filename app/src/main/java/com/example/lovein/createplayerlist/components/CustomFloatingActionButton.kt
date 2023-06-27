d439226bbc9836e67b9fde1774bcd18f0cc0703a
dmatusevich <dmatusevich@smartasset.com>
2023-06-27 15:03:37 +0000

    LI-18: implement closing virtual keyboard to expand more or less icon click

--- app/src/main/java/com/example/lovein/createplayerlist/components/CustomFloatingActionButton.kt
+++ app/src/main/java/com/example/lovein/createplayerlist/components/CustomFloatingActionButton.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
